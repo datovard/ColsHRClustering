a3bd27225519ee6adce35fdf160ac8f86ea2f85b
joagranadosme <joagranadosme@unal.edu.co>
2018-05-19 22:28:37 -0500

    Adding features to Preprocessing and discretizing

--- Discretizing.py
+++ Discretizing.py
@@ -2,6 +2,7 @@ import numpy as np
 from pprint import pprint
 import pandas as pd
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 """
 ID
@@ -48,20 +49,21 @@ class Discretize:
 
     def discretizeFile(self):
 
-        print "DISCRETIZE MODULE"
-
         data = self.dataset
 
-        """
+        print "DISCRETIZE MODULE"
+        print data.dtypes
+        #print data.head()
+        #print data.keys()
+
         #Discretize Age
         bins = 8
-        data["EDAD DEL EMPLEADO"] = pd.cut(data["EDAD DEL EMPLEADO"], bins)
-        print data["EDAD DEL EMPLEADO"].values.value_counts()
-        data["EDAD DEL EMPLEADO"].values.value_counts().plot(kind='bar')
+        column = data["EDAD DEL EMPLEADO"]
+        column = pd.cut(column, bins)
+        print column.values.value_counts()
+        column.values.value_counts().plot(kind='bar', title="EDAD DEL EMPLEADO")
         plt.show()
-        """
-
-        print len(data)
+        data["EDAD DEL EMPLEADO"] = column
 
         #Discretize Salary
         bins = 6
@@ -77,8 +79,84 @@ class Discretize:
 
         merged = pd.concat([range1, range2])
         print merged["SALARIO A 240"].value_counts().sort_index()
-        merged["SALARIO A 240"].value_counts().sort_index().plot(kind='bar')
+        merged["SALARIO A 240"].value_counts().sort_index().plot(kind='bar', title="SALARIO A 240")
+        data["SALARIO A 240"] = merged["SALARIO A 240"]
         plt.show()
+
+        # Discretize Initial Date
+        column = data["FECHA INICIO POSESION"]
+        bins_dt = pd.date_range('2006-01-01', freq='1AS', periods=9)
+        bins_str = bins_dt.astype(str).values
+        labels = ['({}, {}]'.format(bins_str[i - 1], bins_str[i]) for i in range(1, len(bins_str))]
+        column = pd.cut(column.astype(np.int64)//10**9,bins=bins_dt.astype(np.int64)//10**9,labels=labels)
+        print column.values.value_counts()
+        column.values.value_counts().plot(kind='bar', title="FECHA INICIO POSESION")
+        plt.show()
+        data["FECHA INICIO POSESION"] = column
+
+        # Discretize Primera Alta
+        column = data["PRIMERA ALTA"]
+        print min(column)
+        print max(column)
+        bins_dt = pd.date_range('1976-09-28', freq='7AS', periods=7)
+        bins_str = bins_dt.astype(str).values
+        labels = ['({}, {}]'.format(bins_str[i - 1], bins_str[i]) for i in range(1, len(bins_str))]
+        column = pd.cut(column.astype(np.int64) // 10 ** 9, bins=bins_dt.astype(np.int64) // 10 ** 9, labels=labels)
+        print column.values.value_counts()
+        column.values.value_counts().plot(kind='bar', title="PRIMERA ALTA")
+        plt.show()
+        data["PRIMERA ALTA"] = column
+
+        # Discretize Salario
+        bins = 10
+        column = data['HORAS AL MES']
+        column = pd.cut(column, bins)
+        print column.values.value_counts()
+        column.values.value_counts().plot(kind='bar', title='HORAS AL MES')
+        plt.show()
+        data['HORAS AL MES'] = column
+
+        # Salarios Minimos
+        bins = 10
+        column = data["SALARIOS MINIMOS"]
+        column = pd.cut(column, bins)
+        print column.values.value_counts()
+        column.values.value_counts().plot(kind='bar', title="SALARIOS MINIMOS")
+        plt.show()
+        data["SALARIOS MINIMOS"] = column
+
+        # Discretize ES AFILIADO A PAC O TIENE AFILIADO A UN FAMILIAR
+        bins = 7
+        column = data["ES AFILIADO A PAC O TIENE AFILIADO A UN FAMILIAR"]
+        column = pd.cut(column, bins, labels=["0", "1", "2", "3", "4", "5", "6"])
+        print column.values.value_counts()
+        column.values.value_counts().plot(kind='bar', title="ES AFILIADO A PAC O TIENE AFILIADO A UN FAMILIAR")
+        plt.show()
+        data["ES AFILIADO A PAC O TIENE AFILIADO A UN FAMILIAR"] = column
+
+        # Discretize AFILIADO A PAC
+        bins = 2
+        column = data["AFILIADO A PAC"]
+        print "MAX", max(column)
+        print "MIN", min(column)
+        column = pd.cut(column, bins, labels=["0", "1"])
+        print column.values.value_counts()
+        column.values.value_counts().plot(kind='bar', title="AFILIADO A PAC")
+        plt.show()
+        data["AFILIADO A PAC"] = column
+
+        # Discretize AFILIADO A PAC
+        bins = 6
+        column = data["FAMILIAR AFILIADO A PAC"]
+        print "MAX", max(column)
+        print "MIN", min(column)
+        column = pd.cut(column, bins, labels=["0", "1", "2", "3", "4", "5"])
+        print column.values.value_counts()
+        column.values.value_counts().plot(kind='bar', title="FAMILIAR AFILIADO A PAC")
+        plt.show()
+        data["FAMILIAR AFILIADO A PAC"] = column
+
+        #print data
 
         #Renew dataset and return it
         self.dataset = data
--- Preprocessing.py
+++ Preprocessing.py
@@ -1,7 +1,9 @@
 import pandas as pd
-from datetime import datetime
+import numpy as np
+from datetime import datetime, date, time, timedelta
 
 class Preprocess:
+
     def __init__(self, dataset):
         self.dataset = dataset
 
@@ -12,23 +14,37 @@ class Preprocess:
         self.dataset['SALARIOS MINIMOS'] = pd.Series()
         self.dataset['SALARIOS MINIMOS'] = self.dataset['SALARIO A 240']/(737717)
 
-        self.dataset['FECHA INICIO POSESION'] = map( lambda x: datetime.strptime( x, '%d/%m/%Y' ) if x != "00/00/0000" else None, self.dataset['FECHA INICIO POSESION'] )
-        self.dataset['FIN'] = map( lambda x: datetime.strptime( x, '%d.%m.%Y' ) if x != "31.12.9999" and x != "00.00.0000" else None, self.dataset['FIN'] )
-        self.dataset['PRIMERA ALTA'] = map( lambda x: datetime.strptime( x, '%d.%m.%Y' ) if x != "31.12.9999" and x != "00.00.0000" else None, self.dataset['PRIMERA ALTA'] )
-        self.dataset['FECHA EXPIRACION CONTRATO'] = map( lambda x: datetime.strptime( x, '%d.%m.%Y' ) if x != "31.12.9999" and x != "00.00.0000" else None, self.dataset['FECHA EXPIRACION CONTRATO'] )
+        #Missing data in FECHA INICIO POSESION
+        mean = datetime.strptime("2005-01-18", '%Y-%m-%d')
+        self.dataset['FECHA INICIO POSESION'] = map(lambda x: datetime.strptime(x, '%d/%m/%Y') if x != "00/00/0000" else mean, self.dataset['FECHA INICIO POSESION'])
+        print (np.array(self.dataset['FECHA INICIO POSESION'], dtype='datetime64[ns]')).view('i8').mean().astype('datetime64[ns]')
 
+        mean = datetime.strptime("1732-03-18", '%Y-%m-%d')
+        self.dataset['FIN'] = map(lambda x: datetime.strptime( x, '%d.%m.%Y' ) if x != "31.12.9999" and x != "00.00.0000" else mean, self.dataset['FIN'])
+        print (np.array(self.dataset['FIN'], dtype='datetime64[ns]')).view('i8').mean().astype('datetime64[ns]')
 
+        mean = datetime.strptime("2011-04-23", '%Y-%m-%d')
+        self.dataset['PRIMERA ALTA'] = map(lambda x: datetime.strptime( x, '%d.%m.%Y' ) if x != "31.12.9999" and x != "00.00.0000" else mean, self.dataset['PRIMERA ALTA'])
+        print (np.array(self.dataset['PRIMERA ALTA'], dtype='datetime64[ns]')).view('i8').mean().astype('datetime64[ns]')
+
+        mean = datetime.strptime("1793-04-18", '%Y-%m-%d')
+        self.dataset['FECHA EXPIRACION CONTRATO'] = map(lambda x: datetime.strptime( x, '%d.%m.%Y' ) if x != "31.12.9999" and x != "00.00.0000" else mean, self.dataset['FECHA EXPIRACION CONTRATO'])
+        print (np.array(self.dataset['FECHA EXPIRACION CONTRATO'], dtype='datetime64[ns]')).view('i8').mean().astype('datetime64[ns]')
+
+        """
         #erase_vars = ['ID', 'FECHA DE NACIMIENTO', 'CARGO', 'FECHA INICIO POSESION', 'FIN', 'TURNO', 'SUELDO TEXTO', 'SALARIO', 'HORAS AL MES', 'HORAS SEMANALES', 'HORAS DIARIAS', 'HORARIO TRABAJO', 'GRUPO PERSONAL', 'CLASE DE CONTRATO', 'RELACION LABORAL', 'TIPO DE PACTO', 'PRIMERA ALTA', 'FECHA EXPIRACION CONTRATO', 'AREA DE NOMINA', 'CENTRO DE COSTE', 'DIVISION', 'DIVISION PERSONAL', 'SUBDIVISION PERSONAL', 'AREA DE PERSONAL', 'SEXO', 'EDAD DEL EMPLEADO', 'ROL DEL EMPLEADO', 'SALARIO A 240', 'TIPO DE PACTO ESPECIFICO', 'AFILIADO A PAC', 'FAMILIAR AFILIADO A PAC', 'ES AFILIADO A PAC O TIENE AFILIADO A UN FAMILIAR', 'CATEGORIA ESPECIFICA', 'CATEGORIA', 'SALARIOS MINIMOS']
-        erase_vars = ['ID', 'FECHA DE NACIMIENTO', 'CARGO', 'FECHA INICIO POSESION', 'FIN', 'TURNO', 'SUELDO TEXTO',
+        #erase_vars = ['ID', 'FECHA DE NACIMIENTO', 'CARGO', 'FECHA INICIO POSESION', 'FIN', 'TURNO', 'SUELDO TEXTO',
                       'SALARIO', 'HORAS AL MES', 'HORAS SEMANALES', 'HORAS DIARIAS', 'HORARIO TRABAJO',
                       'GRUPO PERSONAL', 'CLASE DE CONTRATO', 'RELACION LABORAL', 'TIPO DE PACTO', 'PRIMERA ALTA',
                       'FECHA EXPIRACION CONTRATO', 'AREA DE NOMINA', 'CENTRO DE COSTE', 'DIVISION', 'DIVISION PERSONAL',
                       'SUBDIVISION PERSONAL', 'AREA DE PERSONAL', 'SEXO', 'ROL DEL EMPLEADO',
                       'SALARIO A 240', 'TIPO DE PACTO ESPECIFICO', 'FAMILIAR AFILIADO A PAC',
                       'ES AFILIADO A PAC O TIENE AFILIADO A UN FAMILIAR', 'CATEGORIA ESPECIFICA']
-        self.dataset.drop( erase_vars, axis=1, inplace=True )
+        #self.dataset = self.dataset[['SALARIOS MINIMOS', 'EDAD DEL EMPLEADO', 'AFILIADO A PAC', 'CATEGORIA']]
+        """
 
-        self.dataset = self.dataset[['SALARIOS MINIMOS', 'EDAD DEL EMPLEADO', 'AFILIADO A PAC', 'CATEGORIA']]
+        erase_vars = ['ID', 'FECHA DE NACIMIENTO', 'SALARIO', 'SUELDO TEXTO', 'HORAS SEMANALES', 'HORAS DIARIAS', 'FIN']
+        self.dataset.drop( erase_vars, axis=1, inplace=True )
 
         self.dataset = self.dataset.drop( self.dataset[self.dataset["EDAD DEL EMPLEADO"] > 70].index )
 
